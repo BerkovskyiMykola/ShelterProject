6183b9d0358f5e3cf866dd11e27ddc32d1988585
Mykola Berkovskyi <nomvodafon@gmail.com>
2021-12-26 00:39:55 +0200

    some changes

--- ShelterProject/ClientApp/src/components/Home.js
+++ ShelterProject/ClientApp/src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import img from "./img/Fluffex.png"
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
       return (
-          <h1 className="NotFound"><b>Welcome to ShelterProject</b></h1>
+          <img src={img} id="bg" alt="" style={{ height: "100%", width: "100%", position: 'fixed', top: 56, left: 0}}/>
       )
   }
 }
